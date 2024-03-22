1ce7d086290c1f5dd254f16ec5462faa01b362a6
angageeva <angageeva@gmail.com>
2024-03-22 12:16:16 +0100

    Have refactored the code

--- imageFeed/Extensions/ImagesListViewExtensions.swift
+++ imageFeed/Extensions/ImagesListViewExtensions.swift
@@ -1,77 +1,78 @@
 import UIKit
-import os.log
 
 extension imagesListViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    }
-    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        guard let image = UIImage(named: photosName[indexPath.row]) else {
+        guard let image = UIImage(named: photoNames[indexPath.row]) else {
             return 0
         }
         let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
         let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
         let imageWidth = image.size.width
         let scale = imageViewWidth / imageWidth
-        
+
         let heightForRow = image.size.height * scale + imageInsets.top + imageInsets.bottom
-        
+
         return heightForRow
     }
 }
 
 extension imagesListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return photosName.count
+        return photoNames.count
     }
  
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
         guard let imageListCell = cell as? ImagesListCell else {
-            os_log("Error: Unable to dequeue reusable cell for identifier: ImagesListCell.reuseIdentifier", log: log, type: .error)
+            imageFeedLog.logError("Error: Unable to dequeue reusable cell")
+
             return UITableViewCell()
         }
-        
         configCell(for: imageListCell, with: indexPath)
+
         return imageListCell
     }
 }
 
 extension imagesListViewController {
-    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
-        guard let image = UIImage(named: "\(indexPath.row)") else {
-            return
-        }
-        cell.cellImage.image = image
-        
+    func getFormattedDate() -> String {
         lazy var dateFormatter: DateFormatter = {
             let formatter = DateFormatter()
             formatter.dateStyle = .long
             formatter.timeStyle = .none
             return formatter
-        }() //todo where i have to put this?
+        }()
         
-        cell.dateLabel.text = dateFormatter.string(from: Date())
+        return dateFormatter.string(from: Date())
+    }
+    
+    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+        guard let image = UIImage(named: "\(indexPath.row)") else { return }
+        cell.cellImage.image = image
         
-        let isEven = indexPath.row % 2 == 0
+        cell.dateLabel.text = getFormattedDate()
+        
+        cell.likeButton.setImage(cellFavoriteImage(index: indexPath.row), for: .normal)
+
+        let gradient = cellGradient()
+        cell.gradientLayerView.layer.sublayers?.forEach { $0.removeFromSuperlayer() }
+        cell.gradientLayerView.layer.insertSublayer(gradient, at: 0)
+        cell.gradientLayer = gradient
+    }
+    
+    func cellFavoriteImage(index: Int) -> UIImage? {
+        let isEven = (index + 1) % 2 == 0
         let buttonImage = isEven ? UIImage(named: "favorites_button_on") : UIImage(named: "favorites_button_off")
-        cell.likeButton.setImage(buttonImage, for: .normal)
-
-        //GRADIENT
-
+        
+        return buttonImage
+    }
+    
+    func cellGradient() -> CAGradientLayer {
         let gradient = CAGradientLayer()
         let finalColor = UIColor.ypBlack.withAlphaComponent(0.2)
         let startColor = UIColor.ypBlack.withAlphaComponent(0.0)
-        
         gradient.colors = [startColor.cgColor, finalColor.cgColor]
-        
-        // Remove existing gradient layer before adding a new one
-        cell.gradientLayerView.layer.sublayers?.forEach { $0.removeFromSuperlayer() }
-        
-        // Set gradient as the background layer of the cellImage
-        cell.gradientLayerView.layer.insertSublayer(gradient, at: 0)
-        
-        // Save the gradient layer reference to update its frame in layoutSubviews
-        cell.gradientLayer = gradient
+
+        return gradient
     }
 }
--- imageFeed/ImagesList/ImagesListCell.swift
+++ imageFeed/ImagesList/ImagesListCell.swift
@@ -2,21 +2,27 @@ import UIKit
 
 final class ImagesListCell: UITableViewCell {
     static let reuseIdentifier = "ImagesListCell"
-    
+    var gradientLayer: CAGradientLayer?
+
     @IBOutlet weak var cellImage: UIImageView!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var likeButton: UIButton!
     @IBOutlet weak var gradientLayerView: UIView!
-    
-    @IBAction func likeButtonClicked(){}
-    
-    var gradientLayer: CAGradientLayer?
+
+    static private let cornerRadius = 16.0
 
     override func layoutSubviews() {
         super.layoutSubviews()
+
+        resizeGradientLayerView()
+    }
+    
+    @IBAction func likeButtonClicked(){}
+    
+    private func resizeGradientLayerView() {
         gradientLayer?.frame = gradientLayerView.bounds
         gradientLayerView.clipsToBounds = true
-        gradientLayerView.layer.cornerRadius = 16
+        gradientLayerView.layer.cornerRadius = ImagesListCell.cornerRadius
         gradientLayerView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
     }
 }
